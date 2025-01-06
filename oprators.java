7b55891318fdee351a0965ad9c2e909ac61c86ba
aryajain-11 <jain2004arya@gmail.com>
2025-01-06 16:30:00 +0530

    initial commit

---
+++ oprators.java
@@ -0,0 +1,19 @@
+public class oprators {
+    public static void main(String [] args){
+        //relational operator  <   >   <=  >=   ==  !=
+        //logical operator    && || !
+        int a = 10;
+        int b = 20;
+        boolean x = true;
+        boolean y = false;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(x&&y);
+        System.out.println(x||y);
+        System.out.println(!x);
+    }
+}
