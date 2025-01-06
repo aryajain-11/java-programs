e63d5282bf746ea4cc6c6d1d4e514f43f20c1e58
aryajain-11 <jain2004arya@gmail.com>
2025-01-06 16:44:31 +0530

    initail commit

---
+++ floattointeger.java
@@ -0,0 +1,11 @@
+public class floattointeger {
+    public static void main (String [] args ){
+        // type casting  () casting operator
+        // converting float to int 
+        int a;
+        float b = 11.2f;
+        a = (int)b;
+        System.out.println(a);
+
+    }
+}
