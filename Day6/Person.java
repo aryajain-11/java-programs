e43f49c4046a3ef5c318c837591013fdf864cf93
aryajain-11 <jain2004arya@gmail.com>
2025-01-11 21:28:43 +0530

    initial commit

--- Day6/Person.java
+++ Day6/Person.java
@@ -2,15 +2,15 @@ public class Person {
     public static void main(String[] args) {
         Person obj1 = new Person();
         obj1.print();
-        Person obj2 = new Person("kinnu" ,12);
+        Person obj2 = new Person("Arya" ,20);
         obj2.print();
     }
     String name;
     int age;
 
     public Person(){
-        name = "Arya";
-        age = 20;
+        name = "Unknown";
+        age = 0;
     }
     public Person(String name, int age ){
         this.name = name;
