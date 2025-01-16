2b77291a5a8ccd3a647eb740d88232bb4c71c466
aryajain-11 <jain2004arya@gmail.com>
2025-01-16 15:14:28 +0530

    Initial commit

---
+++ Day9/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    String name;
+    int age;
+   public  Person(){
+    this.name = "unknowm ";
+    this.age = 0;
+   }
+   public  Person(String name , int age  ){
+    this.name = name;
+    this.age = age;
+   }
+}
+class Student extends Person{
+    int studentId;
+    public Student(String name,int age,int studentId){
+        super(name, age);
+        this.studentId = studentId;
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Student obj = new Student ("Arya",20,574);
+        System.out.println("Name is : " + obj.name);
+        System.out.println("Age  is : " + obj.age);
+        System.out.println("StudentId is : " + obj.studentId);
+        
+
+    }
+}
