f9a0073b0be66be06c85629fb67d4b34e2bc6164
aryajain-11 <jain2004arya@gmail.com>
2025-01-16 15:52:09 +0530

    Initial commit

--- Day6/Animal.java
+++ Day6/Animal.java
@@ -1,27 +1,25 @@
-package Day6;
-
 public class Animal {
     String name;
     String species;
     public Animal(String name,String species){
         this.name = name;
         this.species = species;
-        System.out.println("name is "+ name + "species is " + species);
-
     }
 }
 class Dog extends Animal{
     String breed;
-    public Dog (String breed){
-       super();
+    public Dog (String name,String species,String breed){
+       super(name,species);
        this.breed= breed;
-       System.out.println("Breed is " + breed);
     }
-
 }
 class Demo{
     public static void main(String[] args) {
-        Dog obj = new Dog(Pug);
+        Animal obj = new Animal("cat", "Xyz");
+        System.out.println("name " + obj.name + ",species : " +obj.species);
+        Dog obj2 = new Dog("Dog ", "Pug", "BullDog");
+        System.out.println("Name " + obj2.name + ", Species " + obj2.species + " ,breed " + obj2.breed);
+        
 
         
     }
