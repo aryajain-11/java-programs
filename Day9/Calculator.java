e47314139f5cfd0ccf1a292f45ba68ed7adae211
aryajain-11 <jain2004arya@gmail.com>
2025-01-16 13:25:53 +0530

    initial commit

---
+++ Day9/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+    public static int multiply(int a, int b) { 
+        System.out.println("Product of int multiply is: " + a * b);
+        return a * b;
+    }
+
+    public static double multiply(double a, double b) {
+        System.out.println("Product of double multiply is: " + a * b);
+        return a * b;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Calculator obj = new Calculator();
+        obj.multiply(4, 2);
+        obj.multiply(2.2, 4.0);
+    }
+}
+// javac Calculator.java 
+// java Main
+
