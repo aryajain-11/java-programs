c1e4de192e361852fa6c8b1cfe8477dcefc05759
aryajain-11 <jain2004arya@gmail.com>
2025-01-16 13:35:39 +0530

    Initial commit

---
+++ Day9/Shape.java
@@ -0,0 +1,30 @@
+public class Shape {
+    public static void draw(){
+        System.out.println("Drawing the shape ");
+    }
+}
+class Circle extends Shape{
+    public static void draw(){
+        System.out.println("Drawing the Circle");
+    }
+}
+class Rectangle extends Shape{
+    public static void draw(){
+        System.out.println("Drawing the Rectangle");
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Shape obj1 = new Shape();
+        obj1.draw();
+        Circle obj2 = new  Circle();
+        obj2.draw();
+        Rectangle obj3 = new Rectangle();
+        obj3.draw();
+    }
+   
+    
+}
+
+// javac Shape.java
+// java main
