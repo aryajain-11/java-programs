23b3ff259bbb52f357d4ef06963141272800171d
aryajain-11 <jain2004arya@gmail.com>
2025-01-16 09:31:08 +0530

    Initial commit

---
+++ Day8/Circle.java
@@ -0,0 +1,18 @@
+
+package Shapes;
+public class Circle {
+    double radius;
+    public Circle(){
+        radius = 5.2;
+    }
+    public double  calculateArea(){
+        double Area = 3.14 * radius *radius ;
+        System.out.println("Area is : " + Area );
+        return Area;
+    }
+    public static void main(String[] args) {
+        Circle obj = new Circle();
+        obj.calculateArea();
+    }
+    
+}
