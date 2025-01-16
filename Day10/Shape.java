04f6bbea051e5aedd6948899d53d51af72413d84
aryajain-11 <jain2004arya@gmail.com>
2025-01-16 13:57:28 +0530

    Initial commit

---
+++ Day10/Shape.java
@@ -0,0 +1,35 @@
+public  abstract class Shape {
+    public static void  area(){}
+    public static void display(){
+        System.out.println("Displaying shape details ");
+    }
+}
+class Circle extends Shape {
+    public static void area (){
+        int radius = 4;
+        double circleArea = 3.14 * radius *radius ;
+        System.out.println("area is "+ circleArea);
+    }
+} 
+class Rectangle extends Shape {
+    public static void area (){
+        int length = 5;
+        int width = 6 ;
+        int rectangleArea = length * width;
+        System.out.println("area of rectangle is : "+ rectangleArea );
+    }
+
+}
+class Main {
+    public static void main(String[] args) {
+       // Shape obj1 = new Shape();
+       // obj1.display();
+        Circle obj2 = new Circle ();
+        obj2.area();
+        Rectangle obj3 = new Rectangle();
+        obj3.area();
+        
+
+    }
+}
+// why this code give me error
