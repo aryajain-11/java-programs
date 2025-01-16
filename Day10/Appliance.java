c1db66e77b9da9825c67055f20197f950b4c7a6e
aryajain-11 <jain2004arya@gmail.com>
2025-01-16 17:35:36 +0530

    Initial commit

---
+++ Day10/Appliance.java
@@ -0,0 +1,25 @@
+public abstract class Appliance {
+    public  void  turnOn(){}
+    public  void showBrand() {
+        System.out.println("appliance brand");
+    }
+}
+class  WashingMachine extends Appliance{
+    public  void  turnOn(){
+        System.out.println("Washing Machine is starting");
+    }    
+}
+class Refrigerator extends Appliance{
+    public  void  turnOn(){
+        System.out.println( "Refrigerator is starting" );
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        WashingMachine obj1 = new WashingMachine();
+        obj1.turnOn();
+        Refrigerator obj2 = new Refrigerator();
+        obj2.turnOn();
+    }
+   
+}
