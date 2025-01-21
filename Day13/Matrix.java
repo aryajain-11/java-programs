a3dd6b15fa03ea4b0b22582a4a96a16820d9c36c
aryajain-11 <jain2004arya@gmail.com>
2025-01-21 22:33:56 +0530

    initial commit

---
+++ Day13/Matrix.java
@@ -0,0 +1,13 @@
+public class Matrix {
+    public static void main(String[] args) {
+        int arr [][] = { 
+            {10,20,30},
+            {11,12,13},
+            {41,42,43},
+    };
+    for (int[] is : arr) {
+        System.out.println(arr[is]);
+    }
+    
+}
+}
---
+++ Day13/Student.java
@@ -0,0 +1,48 @@
+public class Student {
+    int rollno;
+    String name;
+    int marks;
+}
+
+class Demo {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.rollno = 1;
+        s1.name = "arya";
+        s1.marks = 90;
+
+        Student s2 = new Student();
+        s2.rollno = 2;
+        s2.name = "Kinnu";
+        s2.marks = 65;
+
+        Student s3 = new Student();
+        s3.rollno = 3;
+        s3.name = "Arpita";
+        s3.marks = 60;
+
+        Student s4 = new Student();
+        s4.rollno = 4;
+        s4.name = "Vanshika";
+        s4.marks = 98;
+
+        Student s5 = new Student();
+        s5.rollno = 5;
+        s5.name = "Palak";
+        s5.marks = 88;
+
+        Student[] stu = new Student[5];
+        stu[0] = s1;
+        stu[1] = s2;
+        stu[2] = s3;
+        stu[3] = s4;
+        stu[4] = s5;
+
+        for (int i = 0; i < stu.length; i++) {
+            if (stu[i].marks > 75) {
+                System.out.println("rollno: " + stu[i].rollno + " name: " + stu[i].name + " marks: " + stu[i].marks);
+            }
+    }
+}
+
+}
