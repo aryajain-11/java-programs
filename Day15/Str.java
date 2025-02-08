8f0d0c0cb14d174326a871fd47a3fc901ed6d3c2
aryajain-11 <jain2004arya@gmail.com>
2025-02-08 23:25:15 +0530

    Initial commit

--- Day15/Str.java
+++ Day15/Str.java
@@ -5,12 +5,33 @@ public class Str {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name : ");
         String name = sc.next();
-        String name1 = name.toUpperCase();
-        System.out.println(name1);
-        System.out.println(name);
+        // uppercase
+        System.out.println(name.toUpperCase());
 
-
-    }
-
-    
+        // reverse
+        String rev = "";
+        for(int i = name.length()-1; i >= 0 ; i--){
+            System.out.print(name.charAt(i) + " ");
+            rev += name.charAt(i);
+        }
+         System.out.println();
+        if(name.equals(rev)){
+            System.out.println(name + " is a pallindrome string ");
+        }
+        else{
+            System.out.println(name + " is not a pallindrome string ");
+        }
+        
+        // count vowel
+        int count = 0 ;
+        for(int i = 0 ; i < name.length();i ++){
+        char ch = name.charAt(i);
+        if(ch=='a' || ch == 'e' || ch=='i' || ch=='o' || ch =='u'){
+            count++;
+        }
+      }
+      System.out.println("number of vowels in the name " + count );
+      
+      sc.close(); 
+   }
 }
