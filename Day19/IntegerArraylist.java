ccd5d87ae1d4f620bbfbc65183e3260c37bd1844
aryajain-11 <jain2004arya@gmail.com>
2025-02-12 08:21:48 +0530

    Initial commit

--- Day19/IntegerArraylist.java
+++ Day19/IntegerArraylist.java
@@ -1,12 +1,17 @@
 import java.util.ArrayList;
+import java.util.Collections;
 public class IntegerArraylist {
     public static void main(String[] args) {
+        ArrayList <Integer> list = new ArrayList<>();
 
         for(int i = 0; i<10;i ++){
-            System.out.println((int)(Math.random() * (100-1+1) + 1));
-
+            int num = (int)(Math.random() * (100-1+1) + 1);
+            System.out.println(num);
+            list.add(num);
         }
-       
+        System.out.println(list);
+        System.out.println("prints the largest : " + Collections.max(list));
+        System.out.println("prints the smallest : " + Collections.min(list));
     }
     
 }
