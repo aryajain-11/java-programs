d56973bf6caeef1e451abd9e67ea6098449a0b47
aryajain-11 <jain2004arya@gmail.com>
2025-02-14 12:36:06 +0530

    Initial commit

---
+++ Day22/TreeSetC.java
@@ -0,0 +1,27 @@
+import java.util.TreeSet;
+
+public class TreeSetC {
+    public static void main(String[] args) {
+        TreeSet <Integer> t = new TreeSet <> ();
+        t.add(40); 
+        t.add(10); 
+        t.add(30); 
+        t.add(50); 
+        t.add(20); 
+        t.add(60); 
+        t.add(70); 
+        t.add(80); 
+        t.add(90); 
+        t.add(100); 
+        System.out.println(t);
+
+        t.remove(30);
+        System.out.println("After Remove :  " + t);
+
+        t.add(25);
+        System.out.println(t);
+ 
+        
+    }
+    
+}
