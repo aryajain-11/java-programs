ef8f11b33741a80cd77a466024209c9087fdbc78
aryajain-11 <jain2004arya@gmail.com>
2025-02-14 22:55:20 +0530

    Initial commit

---
+++ Day23/TreeMapC.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class TreeMapC {
+    public static void main(String[] args) {
+        TreeMap<Integer,String> tm = new TreeMap<>();
+        tm.put(1, "One");
+        tm.put(2, "Two");
+        tm.put(3, "Three");
+        tm.put(4, "Four");
+        tm.put(5, "five");
+
+        System.out.println(tm);
+    }
+    
+}
