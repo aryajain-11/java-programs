c3a4c3b813cc0954953fcddf523301e00fc15835
aryajain-11 <jain2004arya@gmail.com>
2025-02-14 22:43:37 +0530

    Initial commit

--- Day23/PriorityQueueA.java
+++ Day23/PriorityQueueA.java
@@ -3,15 +3,15 @@ import java.util.PriorityQueue;
 public class PriorityQueueA {
     public static void main(String[] args) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(90);
-        pq.add(60);
-        pq.add(80);
-        pq.add(100);
-        pq.add(70);
+        pq.offer(90);
+        pq.offer(60);
+        pq.offer(80);
+        pq.offer(100);
+        pq.offer(70);
         System.out.println(pq);
 
         pq.poll();
-        System.out.println(pq);
+        System.out.println("Highest priority element is : " + pq);
 
     }
     
